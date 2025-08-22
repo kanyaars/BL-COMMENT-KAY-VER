7fc25e88f9adf5c6a0a20fba4555939941528357
Kanyaars <kanyaratanaksros@gmail.com>
2025-08-22 23:33:43 +0700

    Kay Ver

--- lib/tools/colors.py
+++ lib/tools/colors.py
@@ -2,7 +2,7 @@ from colorama import Fore, Style, init
 
 init(autoreset=True)
 
-red   = Fore.RED + Style.BRIGHT
+red   = Style.DIM + Fore.RED
 green = Fore.GREEN + Style.BRIGHT
 white = Fore.WHITE + Style.BRIGHT
 reset = Style.RESET_ALL
